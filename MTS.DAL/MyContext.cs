c75902171cf9656bd12b8cc19e591cf80fa1b858
Ömer Selim <omerselimmahmutoglu@hotmail.com>
2017-09-05 19:30:10 +0300

    Revert "entitiy-db finished"
    This reverts commit 20628492b70357248b23cae0f6fbc4cfc726e16e.

--- MTS.DAL/MyContext.cs
+++ MTS.DAL/MyContext.cs
@@ -1,26 +1,20 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using MTS.Models.Entities;
 using MTS.Models.IdentityModels;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MTS.DAL
 {
-    public class MyContext : IdentityDbContext<ApplicationUser>
+   public class MyContext: IdentityDbContext<ApplicationUser>
     {
+
         public MyContext()
             : base("name=MyCon")
-        { }
-
-        public virtual DbSet<ArizaKaydiDetay> Firmalar { get; set; }
-        public virtual DbSet<ArizaKayit> Urunler { get; set; }
-        public virtual DbSet<Kategori> UrunKategoriler { get; set; }
-    
-        //this.RequireUniqueEmail = true;
+        {
+            //this.RequireUniqueEmail = true;
+        }
     }
 }
-
