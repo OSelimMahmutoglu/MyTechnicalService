20628492b70357248b23cae0f6fbc4cfc726e16e
Ömer Selim <omerselimmahmutoglu@hotmail.com>
2017-09-05 19:29:52 +0300

    entitiy-db finished

--- MTS.DAL/MyContext.cs
+++ MTS.DAL/MyContext.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MTS.Models.Entities;
 using MTS.Models.IdentityModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MTS.DAL
 {
-   public class MyContext: IdentityDbContext<ApplicationUser>
+    public class MyContext : IdentityDbContext<ApplicationUser>
     {
-
         public MyContext()
             : base("name=MyCon")
-        {
-            //this.RequireUniqueEmail = true;
-        }
+        { }
+
+        public virtual DbSet<ArizaKaydiDetay> Firmalar { get; set; }
+        public virtual DbSet<ArizaKayit> Urunler { get; set; }
+        public virtual DbSet<Kategori> UrunKategoriler { get; set; }
+    
+        //this.RequireUniqueEmail = true;
     }
 }
+
