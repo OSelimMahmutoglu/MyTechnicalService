7978982da8f72b74ee38623ec18a6d12a1976805
Ömer Selim <osmahmutoglu@gmail.com>
2017-09-08 11:36:58 +0300

    ayarlama

---
+++ MTS.UI.MVC/Controllers/MusteriController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MTS.UI.MVC.Controllers
+{
+    [Authorize(Roles = "Firma")]
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
